3d6c84ca3a34643c4af9af5ab54d8483560d187a
Álvaro <alvarodefelipefriginal@gmail.com>
2026-03-29 13:04:25 +0200

    True commit

--- src/aplicacion/Ejercicio1.java
+++ src/aplicacion/Ejercicio1.java
@@ -1,8 +1,16 @@
 package aplicacion;
+/* import java.time.Duration;
+import java.time.LocalDateTime; */
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+        /* LocalDateTime inicio = LocalDateTime.now();
+        LocalDateTime fin = LocalDateTime.now(); */
+
         System.out.println(sumarNaturales(7));
+        /* System.out.println("Tiempo ejecución: " + Duration.between(inicio, fin).toMillis() + " ms");
+        System.out.println("Fecha y hora de inicio: " + inicio);
+        System.out.println("Fecha y hora de fin: " + fin); */
     }   
 
     public static int sumarNaturales(int n) {
--- src/aplicacion/Ejercicio1Stream.java
+++ src/aplicacion/Ejercicio1Stream.java
@@ -1,6 +1,8 @@
 package aplicacion;
 
 import java.util.stream.*;
+/* import java.time.Duration;
+import java.time.LocalDateTime; */
 
 public class Ejercicio1Stream {
     public static int sumarNaturales(int n) {
@@ -9,6 +11,12 @@ public class Ejercicio1Stream {
 
     public static void main (String[] args) {
         int n = 7;
+        /* LocalDateTime inicio = LocalDateTime.now();
+        LocalDateTime fin = LocalDateTime.now(); */
+
         System.out.println("Suma de números naturales: " + sumarNaturales(n));
+        /* System.out.println("Tiempo ejecución: " + Duration.between(inicio, fin).toMillis() + " ms");
+        System.out.println("Fecha y hora de inicio: " + inicio);
+        System.out.println("Fecha y hora de fin: " + fin); */
     }
 }
